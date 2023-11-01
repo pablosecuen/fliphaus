b8ae2ad0fe35162a281330c26846d69edcc3168b
pablosecuen <pablosecuen@gmail.com>
2023-11-01 14:33:18 -0300

    added faqs to utils

--- utils.ts
+++ utils.ts
@@ -42,8 +42,54 @@ export const handleSmoothScroll = (event: React.MouseEvent<HTMLAnchorElement, Mo
 
 
   export const tabs = [
-    { label: "Reservar", href: "#reservar" },
-    { label: "Disponibles", href: "/disponibles", onClick: "disponibles" },
-    { label: "Experiencias", href: "#experiencias", onClick: "experiencias" },
-    { label: "Contacto", href: "#contacto", onClick: "contacto" }
+    { label: "Reservar", href: "/reservar" },
+    { label: "Disponibles", href: "/propiedades", onClick: "propiedades" },
+    { label: "Experiencias", href: "/experiencias", onClick: "experiencias" },
   ];
+
+  export const faqs = [
+    {
+      question: "¿En qué moneda están especificadas las tarifas?",
+      answer: "Todas nuestras tarifas están en dólares americanos."
+    },
+    {
+      question: "¿Qué métodos de pago aceptan?",
+      answer: "Los pagos son con tarjeta de crédito o transferencia local o internacional."
+    },
+    {
+      question: "Si utilizo una tarjeta de crédito emitida en Argentina, ¿cuánto pagaré?*",
+      answer: "Si tu tarjeta de crédito fue expedida por un banco argentino, pagarás el equivalente al dólar oficial del día + 75% impuesto país, como todos los gastos en dólares en la República Argentina."
+    },
+    {
+      question: "¿Puedo solicitar una preaprobación para posterior pagar por transferencia?",
+      answer: "Sí, puedes solicitar una preaprobación de 24 horas para realizar una transferencia local o internacional. Si pagas desde una cuenta argentina (en pesos), ten en cuenta que debes pagar el equivalente a la punta más alta del dólar paralelo (azul). Puedes solicitarnos los números de cuenta, una vez solicitada la preaprobación del inmueble deseado."
+    },
+    {
+      question: "¿Puedo reservar por otros canales?",
+      answer: "¡Sí, claro! Encontrarás nuestros anuncios en Airbnb, Booking.com, VRBO, TripAdvisor, HomeToGo, Marriot Homes & Villas y otras alternativas. Ten en cuenta que, en estos canales, pagarás un cargo de servicio adicional."
+    },
+    {
+      question: "¿Pagaré un depósito de seguridad?",
+      answer: "Para todas nuestras propiedades solicitamos un depósito de seguridad entre USD 100 y USD 600 dependiendo del tamaño y capacidad de la propiedad. Este depósito se retiene de tu tarjeta de crédito hasta 7 días después del check-out. Si encontramos algún inventario que falta, te notificaremos y te cargaremos según nuestra lista de precios."
+    },
+    {
+      question: "¿Es posible que el precio de las propiedades varíe en función del número de huéspedes?",
+      answer: "Así es como funciona. Para todas nuestras propiedades, los precios varían según el número de huéspedes. Todas las personas deben estar incluidas en la reserva, excepto los niños menores de 2 años que no pagan adicional."
+    },
+    {
+      question: "¿Hay una tarifa adicional para las mascotas?",
+      answer: "Correcto; cada mascota pagará una tarifa adicional de limpieza de USD 50 por estancia. Esto significa que si viajas con 2 perros, deberás pagar USD 100. Esta tarifa se solicita para garantizar limpieza y saneamiento en todas nuestras propiedades."
+    },
+    {
+      question: "¿Debo presentar documento de identidad o pasaporte?",
+      answer: "Según las leyes vigentes para alquileres temporales en la Ciudad de Buenos Aires, todos los huéspedes deben presentar documento o pasaporte para ser inscritos en el libro de visitas. Esta es una condición, sin excepción, que se aplica a todos los huéspedes, independientemente del canal a través del cual reservan (nuestro sitio web, Airbnb, Booking.com, VRBO u otro)."
+    },
+    {
+      question: "¿Qué es la política de cancelación de Rent House?",
+      answer: "Si los huéspedes cancelan al menos 30 días antes del check-in, reciben un reembolso completo. También pueden obtener un reembolso completo dentro de las 48 horas de la reserva si la cancelación ocurre al menos 14 días antes del check-in. Si cancelan entre 7 y 30 días antes del check-in, usted recibe el importe correspondiente al 50% de todas las noches. Si cancelan menos de 7 días antes del check-in, cobramos el 100% de todas las noches."
+    },
+    {
+      question: "¿Cuál es la política de cambio de fecha de Rent House?",
+      answer: "No se permite cambio de fecha para acortar su estancia. En caso de ser afectado por esta política, le sugerimos cancelar la reserva, siempre que se encuentre dentro de los plazos para recibir un reembolso total o parcial. Si desea prolongar su estancia, puede contactar a su anfitrión para realizar el cambio correspondiente y le enviará el cupón de pago."
+    }
+  ]
