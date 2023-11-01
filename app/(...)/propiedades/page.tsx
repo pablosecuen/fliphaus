d96987770dac4014d28cb0759c305ddab1e39d02
pablosecuen <pablosecuen@gmail.com>
2023-11-01 14:33:54 -0300

    propiedades page

---
+++ app/(...)/propiedades/page.tsx
@@ -0,0 +1,20 @@
+import Filters from "@/app/components/form/filters";
+import SearchForm from "@/app/components/form/search-form";
+import React from "react";
+
+function Propiedades() {
+  return (
+    <div className="min-h-screen pt-24  flex justify-center px-8">
+      <nav className="flex flex-col  border w-1/2">
+        <h2 className="tracking-widest py-8">PROPIEDADES</h2>
+        <SearchForm />
+        <div>
+          <Filters />
+        </div>
+      </nav>
+      <aside className="w-1/2 border"></aside>
+    </div>
+  );
+}
+
+export default Propiedades;
